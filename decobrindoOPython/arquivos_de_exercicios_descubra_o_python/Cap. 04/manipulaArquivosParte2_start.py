3598f745b092254a7805d383234b2d997a7a67df
Pedro Eccel Zimmermann <pedro.eccel.zimmermann@gmail.com>
2022-11-28 15:08:03 -0300

    ta dando algum erro

--- decobrindoOPython/arquivos_de_exercicios_descubra_o_python/Cap. 04/manipulaArquivosParte2_start.py
+++ decobrindoOPython/arquivos_de_exercicios_descubra_o_python/Cap. 04/manipulaArquivosParte2_start.py
@@ -11,4 +11,17 @@ from zipfile import ZipFile
 
 
 def criaZipModo1():
-    shutil.make_archive("ArquivoCompactado", "zip", )
+    shutil.make_archive("ArquivoCompactado", "zip", 'C:\\#Dev\\pedroZimmer\\decobrindoOPython\\arquivos_de_exercicios_descubra_o_python\\Cap. 03' )
+
+#criaZipModo1()
+
+
+def criaZipModo2():
+    with ZipFile("ArquivoZipModo2.zip", "w") as novoZip:
+        novoZip.write("NovoArquivo.bkp")
+        novoZip.write("NovoArquivo.txt")
+        novoZip.write("zipModo1.zip.zip")
+        
+criaZipModo2()
+
+        
