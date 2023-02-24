2413e0d3f1633e13d3f58ae04ad12473d2b64883
Pedro Eccel Zimmermann <pedro.eccel.zimmermann@gmail.com>
2023-02-24 09:07:43 -0300

    top

--- decobrindoOPython/arquivos_de_exercicios_descubra_o_python/Cap. 05/manipulaJSON_start.py
+++ decobrindoOPython/arquivos_de_exercicios_descubra_o_python/Cap. 05/manipulaJSON_start.py
@@ -5,3 +5,22 @@
 import urllib.request  
 import json
 
+def ManipulaJSON():
+    endereco = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson"
+    webURL = urllib.request.urlopen(endereco)
+    if (webURL.getcode() == 200 ):
+        dados = webURL.read()
+        oJSON = json.loads(dados)
+        
+        contagem = oJSON["metadata"]["count"]
+        print("Contagem: " + str(contagem))
+        
+        for local in oJSON["features"]:
+            if local["properties"]["place"] == "47 km S of Faro, Portugal":
+                print("Achei o local")
+            else:
+                print(local["properties"]["place"])
+            
+            
+ManipulaJSON()
+            
