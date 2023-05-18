030a635f01235dd05950a5c1759f56301bbb86c4
Pedro Eccel Zimmermann <pedro.eccel.zimmermann@gmail.com>
2023-05-18 11:05:39 -0300

    aula 146 - Enumercoes

---
+++ CursoJavaUdemy/src/Aula146/application/Program.java
@@ -0,0 +1,29 @@
+package Aula146.application;
+
+
+import Aula146.entities.Order;
+import Aula146.entities.enums.OrderStatus;
+
+import java.util.Date;
+
+
+public class Program {
+    public static void main(String[] args) {
+
+
+        Order order = new Order(1080, new Date(), OrderStatus.PENDING_PAYMENT);
+
+        System.out.println(order);
+
+        OrderStatus os1 = OrderStatus.DELIVERED;
+
+        OrderStatus os2 = OrderStatus.valueOf("DELIVERED");
+
+        System.out.println(os1);
+        System.out.println(os2);
+
+
+
+
+    }
+}
---
+++ CursoJavaUdemy/src/Aula146/entities/enums/OrderStatus.java
@@ -0,0 +1,9 @@
+package Aula146.entities.enums;
+
+public enum OrderStatus {
+
+    PENDING_PAYMENT,
+    PROCESSING,
+    SHIPPED,
+    DELIVERED;
+}
