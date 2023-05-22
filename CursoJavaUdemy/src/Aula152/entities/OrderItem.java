502abb6e7c74b41eb50b0994ee831345ac2c8f81
Pedro Eccel Zimmermann <pedro.eccel.zimmermann@gmail.com>
2023-05-22 11:30:17 -0300

    aula 152 - OrderItem getters e setters

--- CursoJavaUdemy/src/Aula152/entities/OrderItem.java
+++ CursoJavaUdemy/src/Aula152/entities/OrderItem.java
@@ -19,5 +19,19 @@ public class OrderItem {
         return quantity * price;
     }
 
+    public Integer getQuantity() {
+        return quantity;
+    }
 
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
 }
