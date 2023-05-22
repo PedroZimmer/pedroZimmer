fd846827092493df11e943050ade42e5426e3f6c
Pedro Eccel Zimmermann <pedro.eccel.zimmermann@gmail.com>
2023-05-22 11:25:25 -0300

    aula 152 - Program criando

--- CursoJavaUdemy/src/Aula152/application/Program.java
+++ CursoJavaUdemy/src/Aula152/application/Program.java
@@ -1,8 +1,58 @@
 package Aula152.application;
 
+import Aula152.entities.Client;
+import Aula152.entities.Order;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
 public class Program {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
+
+
+        Scanner sc = new Scanner(System.in);
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        System.out.println("Enter client data: ");
+        System.out.print("Name: ");
+        String name = sc.nextLine();
+        System.out.println("Email: ");
+        String email = sc.nextLine();
+        System.out.println("Birth date (DD/MM/YYYY): ");
+        Date birthDate = sdf.parse(sc.next());
+
+        Client client = new Client(name, email, birthDate);
+
+        System.out.println("Enter order data: ");
+        System.out.println("Status: ");
+        String status = sc.nextLine();
+        System.out.println("How many items to this order? ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println("Enter #" + i + " item data: ");
+            System.out.println("Product name: ");
+            sc.nextLine();
+            String productName = sc.nextLine();
+            System.out.println("Product price: ");
+            double productPrice = sc.nextDouble();
+            System.out.println("Quantity: ");
+            int quantity = sc.nextInt();
+        }
+
+        sc.close();
+
+
+
+
+
+
+
+
 
 
 
