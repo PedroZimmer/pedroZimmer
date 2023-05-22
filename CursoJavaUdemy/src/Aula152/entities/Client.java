146b443dfc111e0abfb3fb597dbc1e6967f0a098
Pedro Eccel Zimmermann <pedro.eccel.zimmermann@gmail.com>
2023-05-22 11:11:58 -0300

    aula 152 - Client feito?

--- CursoJavaUdemy/src/Aula152/entities/Client.java
+++ CursoJavaUdemy/src/Aula152/entities/Client.java
@@ -1,4 +1,23 @@
 package Aula152.entities;
 
+import java.util.Date;
+
 public class Client {
+
+    private String name;
+
+    private String email;
+
+    private Date birthDate;
+
+    public Client() {
+        }
+
+    public Client(String name, String email, Date birthDate) {
+        this.name = name;
+        this.email = email;
+        this.birthDate = birthDate;
+
+    }
+
 }
