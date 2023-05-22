833f80d19ccd1f90dd9bd7ee5ed0d32c8f1f0ec0
Pedro Eccel Zimmermann <pedro.eccel.zimmermann@gmail.com>
2023-05-22 11:09:11 -0300

    aula 152 - OrderItem cpa feito

--- CursoJavaUdemy/src/Aula152/entities/OrderItem.java
+++ CursoJavaUdemy/src/Aula152/entities/OrderItem.java
@@ -1,4 +1,25 @@
 package Aula152.entities;
 
 public class OrderItem {
+
+    private Integer quantity;
+
+    private Double price;
+
+
+    public OrderItem() {
+    }
+
+    public OrderItem(Integer quantity, Double price) {
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public Double subTotal(Integer quantity, Double price) {
+        return quantity * price;
+    }
+
+
+    }
+
 }
