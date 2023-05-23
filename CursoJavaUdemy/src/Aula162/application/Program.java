338fc9bef699c471a083bc3aa9532999ae46643d
Pedro Eccel Zimmermann <pedro.eccel.zimmermann@gmail.com>
2023-05-23 09:24:52 -0300

    Aula 162 - scanner

--- CursoJavaUdemy/src/Aula162/application/Program.java
+++ CursoJavaUdemy/src/Aula162/application/Program.java
@@ -1,4 +1,17 @@
 package Aula162.application;
 
+import java.util.Scanner;
+
 public class Program {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number of products: ");
+        int n = sc.nextInt();
+
+
+
+
+    }
 }
