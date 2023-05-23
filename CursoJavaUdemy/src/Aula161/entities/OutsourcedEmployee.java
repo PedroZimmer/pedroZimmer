6dce314949df3feb22575a7e758ce3b16bdbc482
Pedro Eccel Zimmermann <pedro.eccel.zimmermann@gmail.com>
2023-05-23 09:08:14 -0300

    Aula 161 - OutsourcedEmployee pronto

---
+++ CursoJavaUdemy/src/Aula161/entities/OutsourcedEmployee.java
@@ -0,0 +1,21 @@
+package Aula161.entities;
+
+public class OutsourcedEmployee extends Employee{
+
+    private Double additionalCharge;
+
+    public OutsourcedEmployee(){
+        super();
+    }
+
+    public OutsourcedEmployee(String name, Integer hours, Double valuePerHour, Double additionalCharge){
+        super(name, hours, valuePerHour);
+        this.additionalCharge = additionalCharge;
+    }
+
+    @Override
+    public Double payment(){
+        return super.payment() + additionalCharge * 1.1;
+    }
+
+}
