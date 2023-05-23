91f9fd303d478ece43a95b121b2d0d75d948e448
Pedro Eccel Zimmermann <pedro.eccel.zimmermann@gmail.com>
2023-05-23 10:01:33 -0300

    Aula 164 - Shape pronto (abstrata)

---
+++ CursoJavaUdemy/src/Aula164_MethodsAbstracts/entities/Shape.java
@@ -0,0 +1,25 @@
+package Aula164_MethodsAbstracts.entities;
+
+import Aula164_MethodsAbstracts.entities.enums.Color;
+
+public abstract class Shape {
+
+    private Color color;
+
+    public Shape() {
+    }
+
+    public Shape(Color color) {
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public abstract double area();
+}
