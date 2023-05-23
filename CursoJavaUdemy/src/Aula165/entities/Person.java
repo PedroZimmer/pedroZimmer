778f1490bdb9a4f95ce9943d32ee79604a02538d
Pedro Eccel Zimmermann <pedro.eccel.zimmermann@gmail.com>
2023-05-23 10:20:07 -0300

    Aula 165 Exercicio - person feito?

---
+++ CursoJavaUdemy/src/Aula165/entities/Person.java
@@ -0,0 +1,31 @@
+package Aula165.entities;
+
+public abstract class Person {
+
+    private String name;
+    private Double annualIncome;
+
+    public Person() {
+    }
+
+    public Person(String name, Double annualIncome) {
+        this.name = name;
+        this.annualIncome = annualIncome;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getAnnualIncome() {
+        return annualIncome;
+    }
+
+    public void setAnnualIncome(Double annualIncome) {
+        this.annualIncome = annualIncome;
+    }
+}
