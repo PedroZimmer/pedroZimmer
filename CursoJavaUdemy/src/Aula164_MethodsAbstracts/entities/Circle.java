1e8a3b5959d667b4c628a2773e9be10c8470c89a
Pedro Eccel Zimmermann <pedro.eccel.zimmermann@gmail.com>
2023-05-23 10:04:22 -0300

    Aula 164 - circle finished

---
+++ CursoJavaUdemy/src/Aula164_MethodsAbstracts/entities/Circle.java
@@ -0,0 +1,30 @@
+package Aula164_MethodsAbstracts.entities;
+
+import Aula164_MethodsAbstracts.entities.enums.Color;
+
+public class Circle extends Shape{
+
+    private Double radius;
+
+    public Circle(){
+        super();
+    }
+
+    public Circle(Color color, Double radius){
+        super(color);
+        this.radius = radius;
+    }
+
+    public Double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(Double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double area(){
+        return Math.PI * radius * radius;
+    }
+}
