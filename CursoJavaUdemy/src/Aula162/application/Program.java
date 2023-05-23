e8dd94a9645dc7cf36388bf567d2251854b8b5d1
Pedro Eccel Zimmermann <pedro.eccel.zimmermann@gmail.com>
2023-05-23 09:35:25 -0300

    Aula 162 - cpa ta pronto

--- CursoJavaUdemy/src/Aula162/application/Program.java
+++ CursoJavaUdemy/src/Aula162/application/Program.java
@@ -1,19 +1,54 @@
 package Aula162.application;
 
+import Aula162.entities.ImportedProduct;
 import Aula162.entities.Product;
+import Aula162.entities.UsedProduct;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
 
 public class Program {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
 
         Scanner sc = new Scanner(System.in);
         List<Product> list = new ArrayList<>();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
         System.out.println("Enter the number of products: ");
         int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println("Product #" + i + " data:");
+            System.out.println("Common, used or imported (c/u/i)? ");
+            char ch = sc.next().charAt(0);
+            System.out.println("Name: ");
+            sc.nextLine();
+            String name = sc.nextLine();
+            System.out.println("Price: ");
+            double price = sc.nextDouble();
+            if (ch == 'c') {
+                list.add(new Product(name, price));
+            } else if (ch == 'u') {
+                System.out.println("Manufacture date (DD/MM/YYYY): ");
+                Date manufactureDate = sdf.parse(sc.next());
+                list.add(new UsedProduct(name, price, manufactureDate));
+            } else {
+                System.out.println("Customs fee: ");
+                double customsFee = sc.nextDouble();
+                list.add(new ImportedProduct(name, price, customsFee));
+            }
+        }
+
+        System.out.println();
+        System.out.println("PRICE TAGS: ");
+        for (Product product : list) {
+            System.out.println(product.priceTag());
+        }
+
 
 
 
