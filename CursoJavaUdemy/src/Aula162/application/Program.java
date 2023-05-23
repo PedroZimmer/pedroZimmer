5f6b83d567e35fd70b86231b97b536af7d03f8ec
Pedro Eccel Zimmermann <pedro.eccel.zimmermann@gmail.com>
2023-05-23 09:26:08 -0300

    Aula 162 - lista

--- CursoJavaUdemy/src/Aula162/application/Program.java
+++ CursoJavaUdemy/src/Aula162/application/Program.java
@@ -1,11 +1,16 @@
 package Aula162.application;
 
+import Aula162.entities.Product;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        List<Product> list = new ArrayList<>();
 
         System.out.println("Enter the number of products: ");
         int n = sc.nextInt();
