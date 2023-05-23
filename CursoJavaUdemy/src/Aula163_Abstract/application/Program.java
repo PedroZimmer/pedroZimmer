f064a5d6b25e5e7e02ecb2cfc351099918fa5481
Pedro Eccel Zimmermann <pedro.eccel.zimmermann@gmail.com>
2023-05-23 09:44:49 -0300

    Aula 163

---
+++ CursoJavaUdemy/src/Aula163_Abstract/application/Program.java
@@ -0,0 +1,14 @@
+package Aula163_Abstract.application;
+
+import Aula163_Abstract.entities.Account;
+import Aula163_Abstract.entities.BusinessAccount;
+import Aula163_Abstract.entities.SavingsAccount;
+
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Account acc1 = new Account(1001, "Alex", 1000.0);
+    }
+}
