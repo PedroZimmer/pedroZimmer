5b4569c5d5b06280065eabe8c4464253124de7f2
Pedro Eccel Zimmermann <pedro.eccel.zimmermann@gmail.com>
2023-05-23 09:29:52 -0300

    Aula 162 - feito

--- CursoJavaUdemy/src/Aula162/entities/ImportedProduct.java
+++ CursoJavaUdemy/src/Aula162/entities/ImportedProduct.java
@@ -1,4 +1,35 @@
 package Aula162.entities;
 
 public class ImportedProduct extends Product{
+
+    private Double customsFee;
+
+    public ImportedProduct() {
+        super();
+    }
+
+    public ImportedProduct(String name, Double price, Double customsFee) {
+        super(name, price);
+        this.customsFee = customsFee;
+    }
+
+    public Double getCustomsFee() {
+        return customsFee;
+    }
+
+    public void setCustomsFee(Double customsFee) {
+        this.customsFee = customsFee;
+    }
+
+    public Double totalPrice(){
+        return getPrice() + customsFee;
+    }
+
+    @Override
+    public String priceTag() {
+        return getName() + " $ " + String.format("%.2f", totalPrice()) + " (Customs fee: $ " + String.format("%.2f", customsFee) + ")";
+    }
+
+
 }
+
