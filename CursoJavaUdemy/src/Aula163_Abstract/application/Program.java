5e9ed31020f83b359cad9859513e06a5b7ef9faa
Pedro Eccel Zimmermann <pedro.eccel.zimmermann@gmail.com>
2023-05-23 09:50:05 -0300

    Aula 163

--- CursoJavaUdemy/src/Aula163_Abstract/application/Program.java
+++ CursoJavaUdemy/src/Aula163_Abstract/application/Program.java
@@ -4,11 +4,38 @@ import Aula163_Abstract.entities.Account;
 import Aula163_Abstract.entities.BusinessAccount;
 import Aula163_Abstract.entities.SavingsAccount;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Program {
 
     public static void main(String[] args) {
 
-        Account acc1 = new Account(1001, "Alex", 1000.0);
+        List<Account> list = new ArrayList<>();
+
+        list.add(new SavingsAccount(1001, "Alex", 500.00, 0.01));
+        list.add(new BusinessAccount(1002, "Maria", 1000.00, 400.00));
+        list.add(new SavingsAccount(1003, "Bob", 300.00, 0.01));
+        list.add(new BusinessAccount(1004, "Anna", 500.00, 500.00));
+
+        double sum = 0.0;
+        for (Account acc : list) {
+            sum += acc.getBalance();
+        }
+
+        System.out.printf("Total balance: %.2f%n", sum);
+
+        for (Account acc : list) {
+            acc.deposit(10.0);
+        }
+
+        for (Account acc : list) {
+            System.out.printf("Updated balance for account %d: %.2f%n", acc.getNumber(), acc.getBalance());
+        }
+
+
+
+
     }
 }
--- CursoJavaUdemy/src/Aula163_Abstract/entities/Account.java
+++ CursoJavaUdemy/src/Aula163_Abstract/entities/Account.java
@@ -1,6 +1,6 @@
 package Aula163_Abstract.entities;
 
-public class Account {
+public abstract class Account {
 
     private Integer number;
     private String holder;
