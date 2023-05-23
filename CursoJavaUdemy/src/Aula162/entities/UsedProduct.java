d2f46d915accd130dd730bbbefeefae48e296de8
Pedro Eccel Zimmermann <pedro.eccel.zimmermann@gmail.com>
2023-05-23 09:30:45 -0300

    Aula 162 - feito

--- CursoJavaUdemy/src/Aula162/entities/UsedProduct.java
+++ CursoJavaUdemy/src/Aula162/entities/UsedProduct.java
@@ -1,4 +1,30 @@
 package Aula162.entities;
 
+import java.util.Date;
+
 public class UsedProduct extends Product{
+
+    private Date manufactureDate;
+
+    public UsedProduct() {
+        super();
+    }
+
+    public UsedProduct(String name, Double price, Date manufactureDate) {
+        super(name, price);
+        this.manufactureDate = manufactureDate;
+    }
+
+    public Date getManufactureDate() {
+        return manufactureDate;
+    }
+
+    public void setManufactureDate(Date manufactureDate) {
+        this.manufactureDate = manufactureDate;
+    }
+
+    @Override
+    public String priceTag() {
+        return getName() + " (used) $ " + String.format("%.2f", getPrice()) + " (Manufacture date: " + manufactureDate + ")";
+    }
 }
