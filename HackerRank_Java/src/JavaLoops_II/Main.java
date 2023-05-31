d820f951986121fc5d952a982a836fb09ed653ca
Pedro Eccel Zimmermann <pedro.eccel.zimmermann@gmail.com>
2023-05-31 09:47:34 -0300

    .

--- HackerRank_Java/src/JavaLoops_II/Main.java
+++ HackerRank_Java/src/JavaLoops_II/Main.java
@@ -1,2 +1,35 @@
-package JavaLoops_II;public class Main {
+package JavaLoops_II;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] argh) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        ArrayList<List<Integer>> list = new ArrayList<>();
+        for (int i = 0; i < t; i++) {
+            List<Integer> list1 = new ArrayList<Integer>();
+
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            int sum = a;
+            for (int j = 0; j < n; j++) {
+
+                sum += Math.pow(2, j) * b;
+                list1.add(sum);
+            }
+            list.add(list1);
+
+        }
+        for (List<Integer> x : list) {
+            for (Integer element : x) {
+                System.out.print(element + " ");
+            }
+            System.out.println();
+            in.close();
+        }
+    }
 }
--- HackerRank_Java/src/JavaOutputFormatting/Main.java
+++ HackerRank_Java/src/JavaOutputFormatting/Main.java
@@ -1,2 +1,21 @@
-package JavaOutputFormatting;public class Main {
+package JavaOutputFormatting;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("================================");
+        for(int i=0;i<3;i++)
+        {
+            String s1=sc.next();
+            int x=sc.nextInt();
+            System.out.printf("%-15s", s1);
+            System.out.printf("%03d", x);
+
+        }
+        System.out.println("================================");
+
+    }
 }
