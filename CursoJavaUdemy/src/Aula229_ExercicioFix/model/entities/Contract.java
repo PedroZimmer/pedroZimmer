521677adeadaed01ca5c13a8c2f29e99b4e95ee1
Pedro Eccel Zimmermann <pedro.eccel.zimmermann@gmail.com>
2023-06-01 14:25:46 -0300

    Aula 229 - Contract finished

--- CursoJavaUdemy/src/Aula229_ExercicioFix/model/entities/Contract.java
+++ CursoJavaUdemy/src/Aula229_ExercicioFix/model/entities/Contract.java
@@ -1,4 +1,28 @@
 package Aula229_ExercicioFix.model.entities;
 
+import java.util.Date;
+
 public class Contract {
+
+    private Integer number;
+    private Date date;
+    private Double totalValue;
+    private Installment installment;
+
+    public Contract() {
+    }
+
+    public Contract(Integer number, Date date, Double totalValue) {
+        this.number = number;
+        this.date = date;
+        this.totalValue = totalValue;
+    }
+
+    public Contract(Integer number, Date date, Double totalValue, Installment installment) {
+        this.number = number;
+        this.date = date;
+        this.totalValue = totalValue;
+        this.installment = installment;
+    }
+
 }
