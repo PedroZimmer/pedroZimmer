2509ebe077dd72af351f4f5aa3599e45faa7c888
Pedro Eccel Zimmermann <pedro.eccel.zimmermann@gmail.com>
2023-06-01 14:27:48 -0300

    Aula 229 - OnlinePaymentService finished

--- CursoJavaUdemy/src/Aula229_ExercicioFix/model/services/OnlinePaymentService.java
+++ CursoJavaUdemy/src/Aula229_ExercicioFix/model/services/OnlinePaymentService.java
@@ -1,4 +1,8 @@
 package Aula229_ExercicioFix.model.services;
 
 public interface OnlinePaymentService {
+
+    public double paymentFee(double amount);
+    public double interest(double amount, int months);
+
 }
