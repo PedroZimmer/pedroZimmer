8cccc81555f18b6ca8b08dd6deab07fb6ecda8b4
Pedro Eccel Zimmermann <pedro.eccel.zimmermann@gmail.com>
2023-06-01 14:22:24 -0300

    Aula 229 - Program add o programa principal

--- CursoJavaUdemy/src/Aula229_ExercicioFix/application/Program.java
+++ CursoJavaUdemy/src/Aula229_ExercicioFix/application/Program.java
@@ -1,4 +1,28 @@
 package Aula229_ExercicioFix.application;
 
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.Scanner;
+
 public class Program {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        DateTimeFormatter dataFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+
+        System.out.println("Entre com os dados do contrato:");
+        System.out.print("Número: ");
+        int idContrato = sc.nextInt();
+        System.out.print("Data (dd/MM/yyyy): ");
+        String data = sc.next();
+        System.out.print("Valor do contrato: ");
+        double valorContrato = sc.nextDouble();
+        System.out.println("Entre com o número de parcelas: ");
+        int numeroParcelas = sc.nextInt();
+
+
+
+    }
+
 }
