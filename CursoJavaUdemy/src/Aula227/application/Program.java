d988cffac1f1e9a65110a6cd3b408120db8d08e3
Pedro Eccel Zimmermann <pedro.eccel.zimmermann@gmail.com>
2023-06-01 10:55:01 -0300

    Aula 227

---
+++ CursoJavaUdemy/src/Aula227/application/Program.java
@@ -0,0 +1,56 @@
+package Aula227.application;
+
+import Aula227.model.entities.CarRental;
+import Aula227.model.entities.Vehicle;
+import Aula227.model.services.BrazilTaxService;
+import Aula227.model.services.RentalService;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        DateTimeFormatter dataFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+
+        System.out.println("Entre com os dados do aluguel do carro: ");
+        System.out.print("Modelo do carro: ");
+        String model = sc.nextLine();
+        System.out.print("Data e hora de retirada (dd/MM/yyyy HH:mm): ");
+        LocalDateTime start = LocalDateTime.parse(sc.nextLine(), dataFormatter);
+        System.out.print("Data e hora de devolução (dd/MM/yyyy HH:mm): ");
+        LocalDateTime finish = LocalDateTime.parse(sc.nextLine(), dataFormatter);
+
+        CarRental cr = new CarRental(start, finish, new Vehicle(model));
+
+        System.out.print("Entre com o preço por hora: ");
+        double pricePerHour = sc.nextDouble();
+        System.out.print("Entre com o preço por dia: ");
+        double pricePerDay = sc.nextDouble();
+
+        RentalService rentalService = new RentalService(pricePerDay, pricePerHour, new BrazilTaxService());
+
+        rentalService.processInvoice(cr);
+
+        System.out.println("INVOICE: ");
+        System.out.println("Pagamento básico: " + String.format("%.2f", cr.getInvoice().getBasicPayment()));
+        System.out.println("Taxa: " + String.format("%.2f", cr.getInvoice().getTax()));
+        System.out.println("Pagamento total: " + String.format("%.2f", cr.getInvoice().getTotalPayment()));
+
+
+
+
+
+
+        sc.close();
+
+
+    }
+
+}
---
+++ CursoJavaUdemy/src/Aula227/model/services/RentalService.java
@@ -0,0 +1,38 @@
+package Aula227.model.services;
+
+import Aula227.model.entities.CarRental;
+import Aula227.model.entities.Invoice;
+
+import java.time.Duration;
+
+public class RentalService {
+
+    private Double pricePerDay;
+    private Double pricePerHour;
+
+    private TaxService taxService;
+
+    public RentalService(Double pricePerDay, Double pricePerHour, TaxService taxService) {
+        this.pricePerDay = pricePerDay;
+        this.pricePerHour = pricePerHour;
+        this.taxService = taxService;
+    }
+
+    public void processInvoice(CarRental carRental) {
+
+        double minutes = Duration.between(carRental.getStart(), carRental.getFinish()).toMinutes();
+        double hours = minutes / 60.0;
+
+        double basicPayment;
+        if (hours <= 12.0) {
+            basicPayment = Math.ceil(hours) * pricePerHour;
+        } else {
+            basicPayment = Math.ceil(hours / 24.0) * pricePerDay;
+        }
+
+        double tax = taxService.tax(basicPayment);
+
+        carRental.setInvoice(new Invoice(basicPayment, tax));
+
+    }
+}
