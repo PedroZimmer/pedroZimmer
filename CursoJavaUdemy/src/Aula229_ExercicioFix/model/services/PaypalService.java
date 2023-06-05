6eacbbe417f3ae26dea73c1b91b5fde5543a4a75
Pedro Eccel Zimmermann <pedro.eccel.zimmermann@gmail.com>
2023-06-05 08:58:05 -0300

    Aula 229 - PaypalService pronto?

--- CursoJavaUdemy/src/Aula229_ExercicioFix/model/services/PaypalService.java
+++ CursoJavaUdemy/src/Aula229_ExercicioFix/model/services/PaypalService.java
@@ -1,4 +1,15 @@
 package Aula229_ExercicioFix.model.services;
 
-public class PaypalService {
+public class PaypalService implements OnlinePaymentService{
+
+    private static final double PAYMENT_FEE = 0.02; // 2%
+    private static final double MONTHLY_INTEREST = 0.01; // 1%
+
+    public double paymentFee(double amount) {
+        return amount * PAYMENT_FEE;
+    }
+
+    public double interest(double amount, int months) {
+        return amount * MONTHLY_INTEREST * months;
+    }
 }
