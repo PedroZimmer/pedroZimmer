ee06bcbdafaad1abca118bad95b6f3bf55fe31c4
Pedro Eccel Zimmermann <pedro.eccel.zimmermann@gmail.com>
2023-06-26 08:07:13 -0300

    .

--- CursoJavaUdemy/src/Aula214/application/Program.java
+++ CursoJavaUdemy/src/Aula214/application/Program.java
@@ -1,7 +1,30 @@
-package AAbase.application;
+package Aula214.application;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
+
+        File file = new File("C:\\Users\\pedro\\Desktop\\in.txt");
+
+        Scanner sc = null;
+
+try {
+            sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                System.out.println(sc.nextLine());
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        } finally {
+            if (sc != null) {
+                sc.close();
+            }
+        }
+
+
 
     }
 }
