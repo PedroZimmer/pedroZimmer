31edd2cb8465d46613e3aaa0eef9cfdc05bdee31
Pedro Eccel Zimmermann <pedro.eccel.zimmermann@gmail.com>
2023-10-02 19:42:41 -0300

    .

--- CursoJavaUdemy/src/Aula268_teste/Main.java
+++ CursoJavaUdemy/src/Aula268_teste/Main.java
@@ -1,4 +1,14 @@
 package Aula268_teste;
 
+import java.sql.Connection;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        Connection conn = db.DB.getConnection();
+        db.DB.closeConnection();
+
+
+    }
 }
